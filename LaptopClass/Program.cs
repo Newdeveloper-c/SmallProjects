22c919cc04c538de492cfeccfd355f5486c23dde
User <User@NONAME>
2023-06-13 17:59:24 +0500

    Добавьте файлы проекта.

---
+++ LaptopClass/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LaptopClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
